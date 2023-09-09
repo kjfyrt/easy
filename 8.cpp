9fddb2aab5262e7e7a4a1f1e5f13c81dd89e513f
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-09 15:30:57 +0300

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+int itc_min(int min, int min2)
+{
+    if(min<min2)
+    {
+        cout << min << endl;
+    }
+    else {
+        cout << min2 << endl;
+    }
+}
---
+++ 8i.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+int main(){
+    int f, g;
+    cin >> f >> g;
+    itc_min(f, g);
+    return 0;
+    }
