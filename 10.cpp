8f624d977e2551bb8cb82ef68c449ab3c9ae8374
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-09 15:42:24 +0300

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+double itc_fmin(double num, double num2)
+{
+    if(num<num2)
+    {
+        cout << num << endl;
+    }
+    else {
+        cout << num2 << endl;
+    }
+}
---
+++ 10i.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+int main(){
+    double C, D;
+    cin >> C >> D;
+    itc_fmin(C, D);
+    return 0;
+    }
