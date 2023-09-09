4f4dd676e0a81e0ea630bfa86bd5aabf7cc74aaf
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-09 14:08:36 +0300

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+bool itc_iseven(int num){
+    if (num%2==0){
+        cout << "TRUE" << endl;;
+    }
+    else {
+        cout << "FALSE" << endl;
+    }
+
+
+}
---
+++ 6i.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+int main(){
+    int c;
+    cin >> c;
+    itc_iseven(c);
+    return 0;
+
+
+}
