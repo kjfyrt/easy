38896198abda7f952df2f55b5637c389442ef038
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-09 16:35:49 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include "easy.h"
 using namespace std;
 int itc_max(int num, int num2)
 {
     if(num>num2)
     {
-        cout << num << endl;
+        return num;
     }
-    else {
-        cout << num2 << endl;
-    }
+    return num2;
+
 }
