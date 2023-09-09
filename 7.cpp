e9524fc02319854167bd4a3f2961885154cad588
kjfyrt <143996871+kjfyrt@users.noreply.github.com>
2023-09-09 14:58:45 +0300

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+int itc_max(int num, int num2)
+{
+    if(num>num2)
+    {
+        cout << num << endl;
+    }
+    else {
+        cout << num2 << endl;
+    }
+}
---
+++ 7i.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "easy.h"
+using namespace std;
+int main(){
+    int d, e;
+    cin >> d >> e;
+    itc_max(d, e);
+    return 0;
+    }
